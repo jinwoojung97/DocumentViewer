5c5dd83f8d7380e549093631e37d9c5ac79f3d3e
정진우 <wlsdn4808@naver.com>
2022-11-16 13:33:03 +0900

    Feat: NavigationBar, UIActivityViewController 추가 및 공유하기 구현

--- DocumentViewer/ViewController.swift
+++ DocumentViewer/ViewController.swift
@@ -8,59 +8,78 @@
 import PDFKit
 import UIKit
 
+import SnapKit
+import Then
+import RxSwift
+import RxGesture
+
 class ViewController: UIViewController {
-    
+
     let pdfView = PDFView()
     
-    private var pageInfoContainer: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 6
-        view.backgroundColor = UIColor.gray
-        return view
-    }()
+    private var naviBar = UINavigationBar().then {
+        $0.isTranslucent = false
+    }
 
-    private var currentPageLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-        label.textColor = UIColor.white
-        return label
-    }()
+    private var pageInfoContainer = UIView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.layer.cornerRadius = 6
+        $0.backgroundColor = UIColor.gray
+    }
+
+    private var currentPageLabel = UILabel().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        $0.textColor = UIColor.white
+    }
 
     private var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpUI()
+        addComponent()
+        setConstraints()
         initDocument()
         pdfViewSetting()
+        naviBarSetting()
     }
 
     deinit {
-            timer?.invalidate()
+        timer?.invalidate()
+    }
+
+    private func addComponent(){
+        [pdfView, pageInfoContainer, naviBar].forEach(view.addSubview)
+        pageInfoContainer.addSubview(currentPageLabel)
+    }
+
+    private func setConstraints(){
+        naviBar.snp.makeConstraints{
+//            $0.height.equalTo(44)
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
         }
 
-    private func setUpUI() {
-        view.addSubview(pdfView)
-        view.addSubview(pageInfoContainer)
+        pdfView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(naviBar.snp.bottom)
+        }
 
-        pdfView.frame = view.bounds
-        pageInfoContainer.addSubview(currentPageLabel)
+        pageInfoContainer.snp.makeConstraints {
+            $0.top.leading.equalTo(pdfView).inset(20)
+        }
 
-        NSLayoutConstraint.activate([
-            pageInfoContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            pageInfoContainer.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            
-            currentPageLabel.topAnchor.constraint(equalTo: pageInfoContainer.topAnchor, constant: 5),
-            currentPageLabel.leadingAnchor.constraint(equalTo: pageInfoContainer.leadingAnchor, constant: 10),
-            currentPageLabel.trailingAnchor.constraint(equalTo: pageInfoContainer.trailingAnchor, constant: -10),
-            currentPageLabel.bottomAnchor.constraint(equalTo: pageInfoContainer.bottomAnchor, constant: -5),
-        ])
+        currentPageLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(5)
+            $0.leading.trailing.equalToSuperview().inset(10)
+        }
     }
 
     private func initDocument(){
-        NotificationCenter.default.addObserver(self, selector: #selector(handlePageChange), name: .PDFViewPageChanged, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handlePageChange),
+                                               name: .PDFViewPageChanged,
+                                               object: nil)
 
         /// local file
         guard let url = Bundle.main.url(forResource: "pdfsample", withExtension: ".pdf") else { return }
@@ -78,13 +97,21 @@ class ViewController: UIViewController {
         pdfView.autoScales = true
         pdfView.displayMode = .singlePageContinuous
         pdfView.displayDirection = .vertical
+        pdfView.accessibilityNavigationStyle = .combined
+    }
+
+    private func naviBarSetting() {
+        let naviItem = UINavigationItem()
+        naviItem.title = "file name"
+        naviItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        naviItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(action))
+        naviBar.items = [naviItem]
     }
 }
 
 // MARK: - PDFView PDFViewPageChanged
 extension ViewController {
-    @objc
-    private func handlePageChange() {
+    @objc private func handlePageChange() {
         view.bringSubviewToFront(currentPageLabel)
         if let currentPage: PDFPage = pdfView.currentPage,
            let pageIndex: Int = pdfView.document?.index(for: currentPage) {
@@ -104,10 +131,28 @@ extension ViewController {
         timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(whenTimerEnds), userInfo: nil, repeats: false)
     }
 
-    @objc
-    private func whenTimerEnds() {
+    @objc private func whenTimerEnds() {
         UIView.animate(withDuration: 1) {
             self.pageInfoContainer.alpha = 0
         }
+    }
+}
+
+// MARK: - NaviBar Action
+extension ViewController {
+    @objc private func done(){
+        print("던던")
+    }
+
+    @objc private func action(){
+        print("액숀")
+        guard let url = Bundle.main.url(forResource: "pdfsample", withExtension: ".pdf") else { return }
+
+        let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil) // url을 공유해야 한다.
+        activityVC.popoverPresentationController?.sourceView = self.view
+
+        // 공유하기 기능 중 제외할 기능이 있을 때 사용
+        // activityVC.excludedActivityTypes = [UIActivityType.airDrop, UIActivityType.addToReadingList]
+        self.present(activityVC, animated: true, completion: nil)
     }
 }
